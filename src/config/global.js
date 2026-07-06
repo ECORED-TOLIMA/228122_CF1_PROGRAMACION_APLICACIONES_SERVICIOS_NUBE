3e09fd0ac4377f5e479750652f5a576f4709cca7
manuelechavarria <maechavarriaor@gmail.com>
2026-07-06 08:48:45 -0500

    ajustes

--- src/config/global.js
+++ src/config/global.js
@@ -393,7 +393,7 @@ export default {
           centro: 'Centro de Comercio y Servicios - Regional Tolima',
         },
         {
-          nombre: 'Sebastian Trujillo Afanador',
+          nombre: 'Manuel Felipe Echavarria Orozco',
           cargo: 'Desarrollador <em>full stack</em>',
           centro: 'Centro de Comercio y Servicios - Regional Tolima',
         },
